7ed846649eb552b831ac8f788c79d67df076cfa3
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-09-22 12:39:51 +0200

    calculadora

---
+++ scripts/calculator.js
@@ -0,0 +1,43 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    let result;
+
+    switch (operator) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                result = 'No se puede dividir por cero';
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            result = 'Operación no válida';
+    }
+
+    document.getElementById('result').textContent = 'Resultado: ' + result;
+}
+
+function resetForm() {
+    document.getElementById('calculator-form').reset();
+    document.getElementById('result').textContent = '';
+}
+
+document.getElementById('calculator-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    calculate();
+});
+
+document.getElementById('reset').addEventListener('click', function () {
+    resetForm();
+});
